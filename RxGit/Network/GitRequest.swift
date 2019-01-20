20fbdab18acca8bdacba7b3ea214f4128614568f
Hang <hhang@microstrategy.com>
2019-01-20 22:34:52 +0800

    Update GitRequest class

--- RxGit/Network/GitRequest.swift
+++ RxGit/Network/GitRequest.swift
@@ -8,15 +8,25 @@
 
 import UIKit
 import RxSwift
+import Alamofire
 
 protocol GitRequest {
     func start() -> Single<String>
+    var queryJSON: JSONDictionary { get set }
+    var method: HTTPMethod { get set }
 }
 
 class GitRequestImpl: GitRequest {
+    var queryJSON: JSONDictionary = [:]
+    var method: HTTPMethod = .get
+    var baseURL: URL = URL(string: "https://api.github.com/graphql")!
+
     func start() -> Single<String> {
         return Single<String>.create(subscribe: { single in
-            single(.success("a"))
+            AF.request(self.baseURL, method: self.method, parameters: self.queryJSON)
+                .responseJSON(completionHandler: { response in
+                    single(.success("a"))
+                })
             return Disposables.create()
         })
     }
