fb4ee8cec1f4a6ccdf40244c7ed7ff99fe726a03
lyxgjzsya <luoyaox@qq.com>
2019-01-28 21:33:50 +0800

    add sign out

--- RxGit/AppController.swift
+++ RxGit/AppController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class AppController: NSObject {
+class AppController: NSObject, UITabBarControllerDelegate {
     private weak var loginViewController: LoginViewController?
-    private var appController : UIViewController!
+    private var appController : UITabBarController!
     private var userInfo : User?
     
     func appDidFinishLaunching(with window: UIWindow?) {
-        guard let controller = window?.rootViewController else {
+        guard let controller = window?.rootViewController as? UITabBarController else {
             fatalError("...")
         }
         appController = controller
+        appController.delegate = self
     }
     
     func showLogin() {
@@ -35,6 +36,20 @@ class AppController: NSObject {
     
     func didLoginWith(user: User) {
         userInfo = user
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.title?.compare("Logout").rawValue == 0 {
+            let alert = UIAlertController(title: "Do you want to sign out?", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { [weak self] _ in
+                self?.userInfo = nil
+                self?.showLogin()
+            }))
+            alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
+            appController.present(alert, animated: !UIAccessibility.isReduceMotionEnabled)
+            return false
+        }
+        return true
     }
     
     
