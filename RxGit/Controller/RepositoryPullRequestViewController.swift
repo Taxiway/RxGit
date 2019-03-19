42edd69485558222e7a493e5efde71c710c22a44
lyxgjzsya <luoyaox@qq.com>
2019-03-19 11:06:28 +0800

    mock repositoryview

---
+++ RxGit/Controller/RepositoryPullRequestViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RepositoryPullRequestViewController.swift
+//  RxGit
+//
+//  Created by Luo, Yaoxiang on 3/18/19.
+//  Copyright © 2019 RxGit. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryPullRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "currentPageChanged"), object: 1)
+    }
+
+}
